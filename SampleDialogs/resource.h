416ea2769186e919ba0b41c20313ea932c28f5ac
Dale Fugier <dale@mcneel.com>
2012-08-13 16:16:31 -0700

    Added SampleDialogs project

---
+++ SampleDialogs/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by SampleDialogs.rc
+//
+#define IDD_MODAL_DIALOG                9002
+#define IDC_PREVIEW                     10000
+#define IDD_CIRCLEPREVIEW_DIALOG        10000
+#define IDC_RADIUS                      10001
+#define IDC_CENTER_X                    10002
+#define IDC_CENTER_Y                    10003
+#define IDC_CENTER_Z                    10004
+#define IDC_SEP1                        10005
+#define IDC_SEP2                        10006
+#define IDC_EDIT1                       19000
+#define IDD_MODELESS_DIALOG             19000
+#define IDC_EDIT2                       19001
+#define IDC_EDIT3                       19002
+#define IDC_BUTTON1                     19003
+#define IDC_BUTTON2                     19004
+#define IDC_BUTTON3                     19005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        9003
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         9002
+#define _APS_NEXT_SYMED_VALUE           9002
+#endif
+#endif
